37c552c7e61cdb83c65c05641af6992e9626a330
Etiosa22 <64558591+Etiosa22@users.noreply.github.com>
2020-06-05 21:23:56 +0100

    Update and rename HNG-02892 to HNG-02892.js

---
+++ scripts/HNG-02892.js
@@ -0,0 +1,10 @@
+Task2();
+
+full name = "Etiosa Agbonkonkon";
+hngi7 Id = "HNG-02892";
+language = "JavaScript";
+function hngReturn(){
+
+    output = console.log("Hello World, this is "+ full name +" with HNG ID "+ hngi7 Id + " using " + language + " for task 2.".");
+}
+hngReturn();
